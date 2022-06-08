5e096b8d05a2603860f3d80eb95782753c569953
srama995 <104242918+srama995@users.noreply.github.com>
2022-06-08 04:13:41 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -20,26 +20,3 @@ module.exports = withBundleAnalyzer({
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
   }
 })
-
-module.exports = {
-
-  async redirects() {
-    return [
-{
-        source: '/tewestettt',
-        destination: 'https://daily-heroes.info',
-        permanent: true
-      },
-      {
-        source: '/battered-hunting-dog-gave-up-on-life-after-owner-deemed-her-useless-dumped-her',
-        destination: 'https://daily-heroes.info/uncategorized/battered-hunting-dog-gave-up-on-life-after-owner-deemed-her-useless-dumped-her',
-        permanent: true
-      },
-      {
-        source: '/when-he-thinks-no-one-is-looking-a-young-boy-stops-to-hug-two-stray-dogs-on-video',
-        destination: 'https://daily-heroes.info/uncategorized/when-he-thinks-no-one-is-looking-a-young-boy-stops-to-hug-two-stray-dogs-on-video',
-        permanent: true
-      }
-    ];
-  }
-};
